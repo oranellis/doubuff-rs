b4dc40b648ef15d7bc64d4141b9388e3b7151e65
Oran Ellis <oran.mcnair@gmail.com>
2025-02-15 11:57:57 +0000

    feat: Chunked diffed writing to the screen, only prints updates

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,8 +1,8 @@
 use std::io::{stdout, Write};
 
 use crossterm::{
-    cursor::{MoveTo, MoveToNextLine},
-    style::{Attribute, Print, SetAttribute, SetColors},
+    cursor::MoveTo,
+    style::{Attribute, Colors, Print, SetAttribute, SetColors},
     QueueableCommand,
 };
 use terminal_vec2::{vec2, Vec2};
@@ -68,21 +68,76 @@ impl TerminalBuffer {
     }
 
     pub fn flush(&mut self) -> std::io::Result<()> {
-        let mut stdout = stdout();
-        stdout.queue(MoveTo(0, 0))?;
-        for (i, c) in self.scratch_buffer.iter().enumerate() {
-            if i != 0 && i % self.buffer_size.col as usize == 0 {
-                stdout.queue(MoveToNextLine(1))?;
+        // Helper struct to store write chunks
+        struct StyledChunk {
+            pub chars: String,
+            pub position: Vec2,
+            pub colour: Colors,
+            pub bold: bool,
+        }
+
+        // Helper function to create working style chunks
+        fn wsc_from_char_and_index(
+            nc: &StyledChar,
+            index: usize,
+            cols: u16,
+        ) -> Option<StyledChunk> {
+            Some(StyledChunk {
+                chars: nc.character.to_string(),
+                position: vec2!(index as u16 % cols, index as u16 / cols)
+                    .expect("Could not make position vec2"),
+                colour: nc.colour,
+                bold: nc.bold,
+            })
+        }
+
+        // Old buffer
+        let ob_iter = self.buffer.iter();
+        // New buffer
+        let nb_iter = self.scratch_buffer.iter();
+
+        let mut write_chunks: Vec<StyledChunk> = Vec::new();
+        let mut working_styled_chunk: Option<StyledChunk> = None;
+
+        let changed_chars: Vec<Option<&StyledChar>> = ob_iter
+            .zip(nb_iter)
+            .map(|(oc, nc)| Some(nc).filter(|&nchar| nchar != oc))
+            .collect();
+
+        for (index, newchar_option) in changed_chars.into_iter().enumerate() {
+            match (newchar_option, working_styled_chunk.as_mut()) {
+                (Some(nc), None) => {
+                    working_styled_chunk = wsc_from_char_and_index(nc, index, self.buffer_size.col);
+                }
+                (Some(nc), Some(wsc)) => {
+                    if nc.colour != wsc.colour || nc.bold != wsc.bold {
+                        working_styled_chunk.take().map(|sc| write_chunks.push(sc));
+                        working_styled_chunk =
+                            wsc_from_char_and_index(nc, index, self.buffer_size.col);
+                    } else {
+                        wsc.chars.push(nc.character);
+                    }
+                }
+                (None, _) => {
+                    working_styled_chunk.take().map(|sc| write_chunks.push(sc));
+                }
             }
-            if c.bold {
+        }
+        working_styled_chunk.take().map(|sc| write_chunks.push(sc));
+
+        let mut stdout = stdout();
+        for write_chunk in write_chunks {
+            stdout.queue(MoveTo(write_chunk.position.col, write_chunk.position.row))?;
+            if write_chunk.bold {
                 stdout.queue(SetAttribute(Attribute::Bold))?;
             } else {
                 stdout.queue(SetAttribute(Attribute::Reset))?;
             }
-            stdout.queue(SetColors(c.colour))?;
-            stdout.queue(Print(c.character))?;
+            stdout.queue(SetColors(write_chunk.colour))?;
+            stdout.queue(Print(write_chunk.chars))?;
         }
         stdout.flush()?;
+
         self.buffer = std::mem::replace(
             &mut self.scratch_buffer,
             vec![' '.into(); self.buffer_size.col as usize * self.buffer_size.row as usize],
